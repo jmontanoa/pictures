e416845b3a773868b034a04f0db55e76d5c738db
jhocabet <jhocabetdoris@gmail.com>
2014-12-04 22:44:11 -0400

    update upload image tc and steps

--- features/steps/uploadImage_steps.rb
+++ features/steps/uploadImage_steps.rb
@@ -1,17 +1,20 @@
-Given(/^I have an upload image link into my PICS$/) do
+Given(/^I am in the Main page$/) do
     puts "I have an image to upload"
 end
 
 When(/^I click on upload image link$/) do
 	#upload_image,html
-    visit   "http://localhost:4567/upload_image"
-    # puts page.methods
-    click_link "upload image"
+    visit   "http://localhost:4567/upload_image"  
+end
+Then(/^I should see the upload_image form$/) do 
+    expect(page).to have_content "Upload Image"
 end
 
-Then(/^I should see the upload_image form$/) do 
-    visit   "http://localhost:4567/upload_image"
-    expect(page).to have_content "Upload Image"
+When(/^I click on Folder list$/) do
+   select('Folder A', from: 'Folder_list')
+end
+Then(/^I should see the selected Folder in the Folder list$/) do |Folder A|
+    expect(page).to have_selector("Folder A")
 end
 
 When(/^I click "Browse" button in the upload_image form$/) do 
@@ -27,20 +30,20 @@ end
 Then(/^I should see the Pictures library in the File Upload pop up$/) do
     expect(page).to have_content "Pictures library"	
 end
-When(/^I click on an image in the Pictures library pane$/) do
+When(/^I select the image.jpg in the Pictures library pane$/) do
     attach_file('Image', '/Libraries/Pictures/image.jpg')
     click_button "Open"
 end
 Then(/^I should see the image name in the upload_image form$/) do
     expect(page).to have_content "image.jpg"	 
 end
+
 When(/^I click on the "upload" button in the upload_image form$/) do
     click_button "Upload"
 end
 Then(/^I should see the  home page$/) do
     expect(page).to have_content "PICS"
 end
-
 
 
 
