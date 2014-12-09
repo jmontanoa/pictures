8eaf235dfb8e2bd32a13377a1479b3b62953f1ed
Juan Pablo <patomps@icloud.com>
2014-12-08 20:35:56 -0400

    Confirmation link fixed
    Working fine

--- app/app.rb
+++ app/app.rb
@@ -8,34 +8,39 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/pics.db"
 
 class User 
   include DataMapper::Resource 
+
+  has n, :folders 
+  
   property :user_id, Serial 
   property :username, String, :unique => true
   property :password, String
   property :status, Boolean, :default => false
-  property :fullname, String
+  property :name, String
+  property :lastname, String
   property :email, String 
   property :token, BCryptHash, :unique => true, :default => lambda { |r, p| r.username}
-  has n, :pictures
-  has n, :folders
 end 
 
 class Picture 
   include DataMapper::Resource 
+
+  belongs_to :folder, :required => false
+
   property :pic_id, Serial 
   property :folder, String 
   property :tag, String
   property :file_path, String
-  belongs_to :user
-  #belongs_to :folder
 end
 
 class Folder
   include DataMapper::Resource 
+
+  belongs_to :user, :required => false
+  has n, :pictures
+
   property :folder_id, Serial 
   property :name, String 
   property :file_path, String
-  belongs_to :user
-  has n, :pictures
 end 
 
 DataMapper.finalize.auto_upgrade!
@@ -45,12 +50,34 @@ get "/link" do
     redirect "/link.html"
 end
 
-get "/enroll/:token" do
-    redirect "/enroll.html&token=#{params['token']}"
+configure do
+    set :token, ''
 end
 
-post "/enroll.html" do
-    
+get "/enroll/:token" do
+    settings.token = params[:token]
+    redirect "/enroll.html?token=#{settings.token}"
+end
+
+post "/confirm" do
+  user = User.first({:token => settings.token})
+  if user.status == true
+    return "Account already confirmed"
+  else
+    user.name     = params[:name]
+    user.lastname = params[:lastname]
+    user.status   = true
+    user.email    = params[:email]  
+    user.email    = params[:password]
+
+    if user.save
+      session[:name] = user.user_id
+      redirect "/link.html"
+      sleep(3)
+      redirect "/MainPage.html"
+    end
+  end
+      
 end
 
 post '/upload_image' do
@@ -74,6 +101,7 @@ end
 
 post '/users' do 
    users = User.new(params[:users])
+   users.token = users.token.gsub!("/", "0")
 
    if users.save
       session[:name] = users.user_id
